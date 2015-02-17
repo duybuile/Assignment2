fcf6281a4c5d74de4ed609c0674e867f2f55fb0f
duybuile <duybuile@gmail.com>
2015-02-17 17:11:07 +0000

    Final commit
    Edit the comments in cacheSolve

--- cacheSolve.R
+++ cacheSolve.R
@@ -3,12 +3,6 @@
 #(and the matrix has not changed), then cacheSolve should retrieve 
 #the inverse from the cache.
 
-# To test, use the following commands 
-#source("makeCacheMatrix.R")
-#M = makeCacheMatrix(3) (3 is an example of n, it is changable)
-#source("cacheSolve.R")
-#cacheSolve(M)
-
 cacheSolve <- function(M,...){
   
   #The random matrix is created using function createMatrix
